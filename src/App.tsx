03c3d4fe7240ecf03edede99eb9674cd9c6e0870
keerthu1510 <gkeerthana1510@gmail.com>
2024-07-22 13:55:01 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextEditor from './components/TextEditor';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
